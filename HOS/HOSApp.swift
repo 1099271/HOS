0d57157427c7d9307dc0c79a199140eb4a1ef49e
1099271 <1099271@no.com>
2025-01-06 14:11:25 +0800

    init

---
+++ HOS/HOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HOSApp.swift
+//  HOS
+//
+//  Created by Ryan on 2025/1/6.
+//
+
+import SwiftUI
+
+@main
+struct HOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
